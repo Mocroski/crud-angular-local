c04b345d3af3dc98da39db6c99723c6c67cf4c3e
Lucas Mocroski <lucas.mocroski@w-security.intranet>
2023-04-19 16:18:00 -0300

    Validacoes feitas

--- src/app/endereco/endereco.module.ts
+++ src/app/endereco/endereco.module.ts
@@ -6,8 +6,10 @@ import { EditarEnderecoComponent } from './editar-endereco/editar-endereco.compo
 import { InserirEnderecoComponent } from './inserir-endereco/inserir-endereco.component';
 import { ListarEnderecoComponent } from './listar-endereco/listar-endereco.component';
 import { EnderecoService } from './services/endereco.service';
+import { SharedModule } from '../shared';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
 
-
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { EnderecoService } from './services/endereco.service';
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule, 
+    SharedModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [
     EnderecoService
